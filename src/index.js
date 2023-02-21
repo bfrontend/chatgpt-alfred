03ca2a21f7bd6a3304e6d17add5d4e27ce19bcec
webxmsj <webxmsj@163.com>
2023-02-21 13:42:05 +0800

    ✨ feat: 删除dotenv包的使用

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 'use strict'
 const argv = process.argv.slice(2)
-import dotenv from 'dotenv-safe'
 import { ChatGPTAPI } from 'chatgpt'
-dotenv.config()
 
 async function askQuestion(){
   const api = new ChatGPTAPI({
@@ -11,9 +9,9 @@ async function askQuestion(){
   const res = await api.sendMessage(argv[0])
   console.log(JSON.stringify({
     items: [{
-      title: argv[0],
-      subtitle: res.text,
-      valid: false
+      title: res.text,
+      type: 'text',
+      valid: true
     }]
   }))
 }
